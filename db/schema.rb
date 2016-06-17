da5b9ce8e733bc99099bdbc0ecc9278d3dede997
RidaAhmad <rida.ahmad@square63.com>
2016-06-17 17:08:51 +0500

    Products & Seeds: Modified to check Data Seeding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160617062124) do
+ActiveRecord::Schema.define(:version => 20160617070122) do
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.datetime "created_at",                             :null => false
